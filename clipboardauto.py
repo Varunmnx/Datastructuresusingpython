9b73f751c3a93b351d128edc88e22534c1a8fa6a
Varunmnx <varun.cheriyath@gmail.com>
2022-02-17 23:03:00 +0530

    created json type of data

--- clipboardauto.py
+++ clipboardauto.py
@@ -1,7 +1,13 @@
 import sys
+from tkinter import W
 import clipboard
 import json
 
+def save_items(filepath,data):
+    with open(filepath,"w") as f:
+     json.dump(data,f)
+
+save_items('test.json',{"key":"value"})
 
 if len(sys.argv)==2:
   command = sys.argv[1]
@@ -10,5 +16,9 @@ if len(sys.argv)==2:
   if command == "load":
       print("load")
   if command == "list":
-      print("list")    
+      print("list") 
+  else:
+      print("unknown command")      
+else:
+    print("please type a command")      
       
