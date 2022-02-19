1374ab086abc879755119f02d96eb40bf1bc9c83
Varunmnx <varun.cheriyath@gmail.com>
2022-02-19 23:43:27 +0530

    added tech tim

--- clipboardauto.py
+++ clipboardauto.py
@@ -1,27 +1,45 @@
 import sys
-from tkinter import W
 import clipboard
 import json
 
-def save_items(filepath,data): # create json
-    with open(filepath,"w") as f:
-     json.dump(data,f)
 
-def load_json(filepath): # read from that json
-    with open(filepath,"r") as f:
-        data =json.load(f)
-        return data #json as python dictionary
+SAVED_DATA = "clipboard.json"
 
-if len(sys.argv)==2:
-  command = sys.argv[1]
-  if command == "save":
-      print("save")
-  if command == "load":
-      print("load")
-  if command == "list":
-      print("list") 
-  else:
-      print("unknown command")      
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("Data saved!")
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key does not exist.")
+    elif command == "list":
+        print(data)
+    else:
+        print("Unknown command")
 else:
-    print("please type a command")      
-      
+    print("Please pass exactly one command.")
+
