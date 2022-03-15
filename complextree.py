dab07a9a1ec8cdfc3ec60b974436df7cc0f72607
Varunmnx <varun.cheriyath@gmail.com>
2022-03-15 12:05:54 +0530

    tree  complex2

--- complextree.py
+++ complextree.py
@@ -1,45 +1,83 @@
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
 
-class Node:
-   def __init__(self, data):
-      self.left = None
-      self.right = None
-      self.data = data
-# Insert Node
-   def insert(self, data):
-      if self.data:
-         if data < self.data:
-            if self.left is None:
-               self.left = Node(data)
-            else:
-               self.left.insert(data)
-         else data > self.data:
-            if self.right is None:
-               self.right = Node(data)
-            else:
-               self.right.insert(data)
-      else:
-         self.data = data
-# Print the Tree
-   def PrintTree(self):
-      if self.left:
-         self.left.PrintTree()
-      print( self.data),
-      if self.right:
-         self.right.PrintTree()
-# Inorder traversal
-# Left -> Root -> Right
-   def inorderTraversal(self, root):
-      res = []
-      if root:
-         res = self.inorderTraversal(root.left)
-         res.append(root.data)
-         res = res + self.inorderTraversal(root.right)
-      return res
-root = Node(27)
-root.insert(14)
-root.insert(35)
-root.insert(10)
-root.insert(19)
-root.insert(31)
-root.insert(42)
-print(root.inorderTraversal(root))
+
+node1 = BinaryTreeNode(50)
+node2 = BinaryTreeNode(20)
+node3 = BinaryTreeNode(45)
+node4 = BinaryTreeNode(11)
+node5 = BinaryTreeNode(15)
+node6 = BinaryTreeNode(30)
+node7 = BinaryTreeNode(78)
+
+node1.leftChild = node2
+node1.rightChild = node3
+node2.leftChild = node4
+node2.rightChild = node5
+node3.leftChild = node6
+node3.rightChild = node7
+
+print("Root Node is:")
+print(node1.data)
+
+print("left child of the node is:")
+print(node1.leftChild.data)
+
+print("right child of the node is:")
+print(node1.rightChild.data)
+
+print("Node is:")
+print(node2.data)
+
+print("left child of the node is:")
+print(node2.leftChild.data)
+
+print("right child of the node is:")
+print(node2.rightChild.data)
+
+print("Node is:")
+print(node3.data)
+
+print("left child of the node is:")
+print(node3.leftChild.data)
+
+print("right child of the node is:")
+print(node3.rightChild.data)
+
+print("Node is:")
+print(node4.data)
+
+print("left child of the node is:")
+print(node4.leftChild)
+
+print("right child of the node is:")
+print(node4.rightChild)
+
+print("Node is:")
+print(node5.data)
+
+print("left child of the node is:")
+print(node5.leftChild)
+
+print("right child of the node is:")
+print(node5.rightChild)
+
+print("Node is:")
+print(node6.data)
+
+print("left child of the node is:")
+print(node6.leftChild)
+
+print("right child of the node is:")
+print(node6.rightChild)
+
+print("Node is:")
+print(node7.data)
+
+print("left child of the node is:")
+print(node7.leftChild)
+
+print("right child of the node is:")
