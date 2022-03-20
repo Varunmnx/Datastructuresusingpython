2902b86594ff24eb879b8764ec3c891e91160e88
Varunmnx <varun.cheriyath@gmail.com>
2022-03-20 19:34:28 +0530

    effective search

--- efficiencydepended.py
+++ efficiencydepended.py
@@ -1,16 +1,74 @@
-def isok():
-    pass
-def finder():
-    r = 1000
-    l =1
-    mid =0
-    while l<r:
-        mid= (l+r)//2
-        if isok(mid):
-           if not isok(mid-1) :
-               return mid
-           else:
-              r= mid-1
-        else:
-            r = mid + 1
+from multiprocessing.sharedctypes import Value
 
+
+class Solution:
+	def isBadVersion(mid):
+           pass
+    
+def firstBadVersion(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		left = 1
+		right = n
+    
+		while left <= right:
+			mid = int((left + right) / 2)
+        
+			if self.isBadVersion(mid):
+				if not self.isBadVersion(mid - 1):
+					return mid
+				else:
+					right = mid - 1
+			else:
+				left = mid + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                # left =1 
+                # right = n
+                # mid = 0
+                # while left<right :
+                #     mid = (left +right )//2
+                #     if value == arr[mid]:
+                #         if not value == arr[mid-1]:
+                #             return mid
+                #         else:
+                #             return right = mid -1    
+                #     else:
+                #         return right = mid+1
+                # left = 1
+                # right = len(array)
+                # mid = 0
+                # while left<right :
+                #     mid = left +right   
+                #     if value == arr[mid]:
+                #         if not value == arr[mid-1]:
+                #             return mid 
+                #         else :
+                #             return right = mid-1                                
+                #     else:
+                #         return right = mid+1
