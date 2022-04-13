d00b22534984e643d0e839a6677f073144c6a464
Varunmnx <varun.cheriyath@gmail.com>
2022-04-13 21:18:52 +0530

    green

--- try.py
+++ try.py
@@ -1,13 +1,13 @@
 #i like the green
 d= set()
 l=0 # length of eachstring
-s ="abcdefabcabcdefg"
+s ="abcdefabcabcdefg" # l will be at beginning 
 res = 0
 for i in range(len(s)):
      while s[i] in d:
          d.remove(s[i])
          l+=1
      d.add(s[i])
-              
-
+     res = max(res,i-l+1)              
+print(res)
          
