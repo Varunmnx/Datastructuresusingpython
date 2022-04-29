004f15babbf9a1991f2e11ee9a68580b5a110780
Varunmnx <varun.cheriyath@gmail.com>
2022-04-29 20:14:19 +0530

    leetcode valid parenthisis2

---
+++ backtracking2.py
@@ -0,0 +1,25 @@
+result = []
+paren = []
+n =3
+
+def backTrack(leftCount, rightCount):
+    if leftCount > n:
+
+        return
+
+    if rightCount > leftCount:
+        return
+
+    if leftCount == rightCount == n:
+        result.append("".join(paren))
+        # return
+
+    paren.append("(")
+    backTrack(leftCount + 1, rightCount)
+    paren.pop()
+
+    paren.append(")")
+    backTrack(leftCount, rightCount + 1)
+    paren.pop()
+backTrack(0,0)
+print(result)
