b57b1afba9dfe4a838d16ba4b6fba1943526bfbe
Varunmnx <varun.cheriyath@gmail.com>
2022-05-12 23:00:24 +0530

    added solution to countandsay

---
+++ countandsay.py
@@ -0,0 +1,17 @@
+n=6
+s='1'
+ans=""
+if n==1: print("1")
+for j in range(n-1):
+    c,i,l=1,0,len(s)
+    while(i<l):
+        if i!=l-1 and s[i]==s[i+1]:
+            c+=1
+        else:
+            ans+=str(c)+s[i]
+            c=1
+        i+=1
+    s=ans
+    # print(s)
+    ans=''
+print(s)
