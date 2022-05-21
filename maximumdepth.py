a19cf073a5f2fc186997e9eb1b56a1349c6ee314
Varunmnx <varun.cheriyath@gmail.com>
2022-05-21 23:25:56 +0530

    binary  maximum depth

---
+++ maximumdepth.py
@@ -0,0 +1,35 @@
+class Btree:
+    def __init__(self,val):
+        self.val = val
+        self.left =None
+        self.right =None
+    def insert(self,data):
+        if self.val:
+            if data<self.val:
+                if not self.left:
+                    self.left = Btree(data)
+                else:
+                    self.left.insert(data) 
+
+            if data<self.val:
+                if not self.right:
+                    self.right = Btree(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.val = Btree(data)             
+    def maximum_depth(self,root):
+                 if not root:
+                     return 0
+                 return (1+ max(self.maximum_depth(root.left),self.maximum_depth(root.right)))      
+
+
+if __name__ =="__main__":
+   Root = Btree(100)
+   Root.left =Btree(90)
+   Root.right =Btree(110)
+   Root.insert(111)
+   Root.insert(55)
+   Root.insert(26)
+   Root.insert(18)
+   Root.maximum_depth(Root)
